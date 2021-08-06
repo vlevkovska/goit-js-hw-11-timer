fea4c1777afbdd907d00faa1fbabbb2df030f034
Valentyna Levkovska <78691453+vlevkovska@users.noreply.github.com>
2021-08-06 12:17:30 +0300

    task fulfilled

---
+++ index.js
@@ -0,0 +1,69 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ targetDate, onTick }) {
+    this.targetDate = targetDate;
+    this.onTick = onTick;
+  }
+  start() {
+    const startTime = Date.parse(this.targetDate);
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      const timeBack = getTimeComponents(deltaTime);
+
+      this.onTick(timeBack);
+    }, 1000);
+  }
+}
+
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 09, 2021'),
+  onTick: updateCountdownTimerFace,
+});
+countdownTimer.start();
+function getTimeComponents(time) {
+  /*
+   * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+   */
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+  /*
+   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+   * остатка % и делим его на количество миллисекунд в одном часе
+   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+   */
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  /*
+   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+   */
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+  /*
+   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+   * миллисекунд в одной секунде (1000)
+   */
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  return { days, hours, mins, secs };
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateCountdownTimerFace({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
